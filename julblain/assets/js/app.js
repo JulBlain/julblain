a2f55fc0192daac8df1163da0ed0dd8856628401
julienblain <blain.julien@gmail.com>
2018-12-17 20:40:51 +0100

    pink arrow home

--- julblain/assets/js/app.js
+++ julblain/assets/js/app.js
@@ -8,6 +8,7 @@
 // any CSS you require will output into a single scss file (app.scss in this case)
 require('../scss/app.scss');
 
+
 // Need jQuery? Install it with "yarn add jquery", then uncomment to require it.
 // var $ = require('jquery');
 
