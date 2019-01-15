aefe37fc9517feb4336167a7ea208f5255c09265
julienblain <blain.julien@gmail.com>
2019-01-15 20:58:08 +0100

    homePage: svg btn

--- julblain/public/js/home/meHomeBtnCircle.js
+++ julblain/public/js/home/meHomeBtnCircle.js
@@ -1,20 +1,56 @@
-let svgHomeMeNF = document.getElementById('pathLanguages');
-let maskMe = document.getElementById('homeMaskBoxLanguages');
-let iSvgBtn = 0;
+let svgHomeMeBtnLanguages = document.getElementById('pathLanguages');
+let svgCms = document.getElementById('pathLibrairy');
+let svgTool = document.getElementById('pathTool');
+
+let maskLanguages = document.getElementById('homeMaskBoxLanguages');
+let maskCms = document.getElementById('homeBoxLibrairy');
+let maskTool = document.getElementById('homeBoxTool');
+
+let iSvgBtnL = 0;
+let iSvgBtnC = 0;
+let iSvgBtnT = 0;
+
+let opacity =1;
+
+
 
 function svgHomeMe() {
-    if(iSvgBtn === 0) {
-        $(maskMe).css("transform", "scale(1)");
-        iSvgBtn++;
+    if(iSvgBtnL === 0) {
+        $(maskLanguages).css("transform", "scale(1)");
+        //fadeOut(svgCms, svgTool);
+        svgCms.style.display = 'none ';
+        iSvgBtnL++;
+
     }
     else {
-        $(maskMe).css("transform", "scale(0)");
-        iSvgBtn--;
+        $(maskLanguages).css("transform", "scale(0)");
+        iSvgBtnL--;
     }
 }
 
 
-$(svgHomeMeNF).click(function() {
-    svgHomeMe();
+$(svgHomeMeBtnLanguages).click(function() {
 
+    svgHomeMe();
 });
+
+function fadeOut(div1, div2) {
+    intervalFadeOut = setInterval(hide, 100);
+
+    function hide() {
+
+        if(opacity > 0) {
+            opacity = opacity - 0.1;
+            div1.style.opacity = opacity;
+            div2.style.opacity = opacity;
+            console.log(opacity);
+        }
+        else {
+            clearInterval(intervalFadeOut);
+        }
+
+    }
+}
+
+
+
